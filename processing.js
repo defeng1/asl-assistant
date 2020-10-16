911a4b4897480ea0b9d3f4fd21586c7c6e8206c3
seleanridley <ridley.s@husky.neu.edu>
2020-10-16 15:11:52 -0400

    Fixed start session buttion

--- processing.js
+++ processing.js
@@ -3,7 +3,9 @@ let button;
 let photo;
 
 function setup() {
+  
   createCanvas(displayWidth, displayHeight);
+  
   capture = createCapture(VIDEO);
   capture.size(500, 500);
   capture.hide();
@@ -13,7 +15,10 @@ function setup() {
   button.position(displayWidth * .1, displayHeight * .9);
   button.mousePressed(saveFrame);
   fill(100);
+
 }
+
+
 
 function draw() {
   background(255);
@@ -22,6 +27,7 @@ function draw() {
                  displayHeight * .1, 
                  displayWidth * .6, 
                  displayHeight * .7);
+        
 
   /* textSize(16);
   textAlign(CENTER);
@@ -30,8 +36,20 @@ function draw() {
 
 //function mousePressed() {
 function saveFrame() {
-    //saveFrames('out', 'png', 1, 30);
-    let c = capture.get(0, 0, 100, 100);
-    photo = image(c, 0, 0);
-    save(photo, 'photo.png');
+
+  /*let c = get(displayWidth * .1, 
+                      displayHeight * .1, 
+                      displayWidth * .6, 
+                      displayHeight * .7);  // 
+  let pic = image(c, 0, 0);
+  save(c, 'output.png'); */
+  let c = get();
+
+  c.save('outputPhoto','png');
+
+
+  console.log(typeof(c))
 }
+
+
+
