b219e9fc29d981b4ab8ef7f870f76b01a994d5be
Jing Chen <jcdevspace@gmail.com>
2020-12-06 14:32:35 -0500

    drawing hand landmarks

--- static/js/processing.js
+++ static/js/processing.js
@@ -11,6 +11,10 @@ let myFont;
 //let json_out = {};
 let interv2;
 
+let hp;
+let poses = [];
+
+
 function setup() {
 
   //load();
@@ -18,6 +22,17 @@ function setup() {
   
   strokeWeight(5)
   capture = createCapture(VIDEO);
+
+  hp = ml5.handpose(capture, modelReady);
+
+  // hp.on('predict', function(results) {
+  //   poses = results;
+  //   // console.log(poses)
+  // });
+
+  hp.on("predict", results => {
+    poses = results;
+  });
 
   capture.hide();
 
@@ -50,6 +65,10 @@ function setup() {
       
 }
 
+function modelReady() {
+  console.log("Model loaded");
+  // hp.singlePose();
+}
 
 function draw() {
 
@@ -101,7 +120,9 @@ function draw() {
     st_button.style('color', 'white');
   }
 
-
+  // Drawing hand landmarks
+  drawKeypoints();
+  drawSkeleton();
 }
 
 
@@ -185,5 +206,71 @@ function saveFrame() {
   
   //runModel(imageData);
   return cleaned;
+}
+
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints()  {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    // let pose = poses[i].pose;
+    // for (let j = 0; j < pose.keypoints.length; j++) {
+    //   // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+    //   let keypoint = pose.keypoints[j];
+    //   // Only draw an ellipse is the pose probability is bigger than 0.2
+    //   if (keypoint.score > 0.2) {
+    //     fill(255, 0, 0);
+    //     noStroke();
+    //     ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+    //   }
+    // }
+    let pose = poses[i];
+    // console.log(pose)
+    for (let j = 0; j < pose.landmarks.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.landmarks[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      // if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint[0], keypoint[1], 10, 10);
+      // }
+    }
+  }
+}
+
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let annotations = poses[i].annotations;
+    // For every skeleton, loop through all body connections
+    stroke(255, 0, 0);
+    for (let j = 0; j < annotations.thumb.length - 1; j++) {
+      // let partA = annotations.thumb[j][0];
+      // let partB = annotations.thumb[j][1];
+      line(annotations.thumb[j][0], annotations.thumb[j][1], annotations.thumb[j + 1][0], annotations.thumb[j + 1][1]);
+    }
+    for (let j = 0; j < annotations.indexFinger.length - 1; j++) {
+      line(annotations.indexFinger[j][0], annotations.indexFinger[j][1], annotations.indexFinger[j + 1][0], annotations.indexFinger[j + 1][1]);
+    }
+    for (let j = 0; j < annotations.middleFinger.length - 1; j++) {
+      line(annotations.middleFinger[j][0], annotations.middleFinger[j][1], annotations.middleFinger[j + 1][0], annotations.middleFinger[j + 1][1]);
+    }
+    for (let j = 0; j < annotations.ringFinger.length - 1; j++) {
+      line(annotations.ringFinger[j][0], annotations.ringFinger[j][1], annotations.ringFinger[j + 1][0], annotations.ringFinger[j + 1][1]);
+    }
+    for (let j = 0; j < annotations.pinky.length - 1; j++) {
+      line(annotations.pinky[j][0], annotations.pinky[j][1], annotations.pinky[j + 1][0], annotations.pinky[j + 1][1]);
+    }
+
+    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.thumb[0][0], annotations.thumb[0][1]);
+    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.indexFinger[0][0], annotations.indexFinger[0][1]);
+    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.middleFinger[0][0], annotations.middleFinger[0][1]);
+    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.ringFinger[0][0], annotations.ringFinger[0][1]);
+    line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.pinky[0][0], annotations.pinky[0][1]);
+  }
 }
 
